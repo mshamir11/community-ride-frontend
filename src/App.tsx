2a712f401f4ad3d125a9c4832f820a5c698bb887
mshamir11 <mshamir11@gmail.com>
2023-02-28 07:08:58 +0530

    Added readme.md

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// TODO Testing out TODO
 function App() {
   return (
     <div className="App">
-        Hi, This is Mohamed Shamir here 👋. Let's build Community Ride.
+        Hi, This is Mohamed Shamir here 👋. Let's build Community Ride. 
     </div>
   );
 }
