cfb4d66e8cc82318204ead5b9a7fccdf54b86f34
mshamir11 <mshamir11@gmail.com>
2023-04-26 06:46:56 +0530

    Added image to the landing page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,40 +1,37 @@
-import React from 'react';
+import Rides from "./Rides";
 
-import './App.css';
-import Rides from './Rides';
-
-// TODO Testing out TODO
 function HomePage() {
-    return (
-        <div className="community-ride">
-            <div className="main">
-                <div className="title">
-                    <h1>Community Ride</h1>
-                </div>
-            </div>
+	return (
+		<div className="community-ride">
+			<section id="hero">
+				<div className="container max-w-6xl mx-auto px-6 py-12">
+					<div className="max-w-lg mx-auto mt-32 mb-32 p-4 font-sans text-5xl text-white uppercase border-4 text-center font-bold backdrop-blur-sm">
+						Community Rides
+					</div>
+				</div>
+			</section>
 
-            <div className="upcoming-rides rides">
-                <h2>Upcoming Rides</h2>
-                {/* TODO Create a carousel for the component */}
-                <div className="ride-components">
-                    {/* TODO Create a html component for the ride details */}
-                    {getRideComponents(5)}
-                </div>
-
-            </div>
-            <div className="past-rides rides">
-                <h2>Past Rides</h2>
-            </div>
-        </div>
-    );
+			<div className="upcoming-rides rides">
+				<h2>Upcoming Rides</h2>
+				{/* TODO Create a carousel for the component */}
+				<div className="ride-components">
+					{/* TODO Create a html component for the ride details */}
+					{getRideComponents(5)}
+				</div>
+			</div>
+			<div className="past-rides rides">
+				<h2>Past Rides</h2>
+			</div>
+		</div>
+	);
 }
 
 function getRideComponents(count: number) {
-    var elements = [];
-    for (let i = 1; i < count; i++) {
-        elements.push(<Rides component_number={i} />)
-    }
-    return elements
+	var elements = [];
+	for (let i = 1; i < count; i++) {
+		elements.push(<Rides component_number={i} />);
+	}
+	return elements;
 }
 
 export default HomePage;
--- src/pages/Rides.tsx
+++ src/pages/Rides.tsx
@@ -1,13 +1,26 @@
+function Rides(props: any) {
+	// console.log(props)
+	const component_number = props.component_number;
 
-function Rides (props:any) {
-    // console.log(props)
-    const component_number = props.component_number;
-    
-
-    return (
-
-        <span>Component {component_number}</span>
-    )
+	return (
+		<div className="">
+			<div className="max-w-md mx-auto rounded-md overflow-hidden shadow-md">
+				<img className="w-full h-48 object-cover" src="[Route Map Image URL]" alt="Route Map" />
+				<div className="px-4 py-3 bg-white">
+					<h3 className="text-lg font-medium text-gray-700 mb-2">[Ride Name]</h3>
+					<p className="text-sm text-gray-500 mb-1">[Ride Date]</p>
+					<p className="text-sm text-gray-500 mb-4">
+						<span className="font-medium">Total Members:</span> [Total Members Joined the Ride]
+					</p>
+					<div className="flex justify-end">
+						<a href="#" className="text-blue-500 font-medium">
+							View Details
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Rides;
